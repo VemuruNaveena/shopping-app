5aa8ed3ea340c1423fc29210608b5f8a4d6d6e47
Naveena Vemuru <naveenavemuru@gmail.com>
2025-03-13 21:18:21 -0400

    adding token in service layer

--- src/app/udemy.service.ts
+++ src/app/udemy.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,11 +6,28 @@ import { inject, Injectable } from '@angular/core';
 })
 export class UdemyService {
   http = inject(HttpClient);
+
+  //
   createCourseApi(course: any) {
+    // logic to add custom token.
+    const headers = new HttpHeaders({
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2NhNGNjNzE1NDAxYzJiMThiMzljMjkiLCJpYXQiOjE3NDE5MTM3OTR9.garlF-MVF80oTngqbsXLUuEM5FLRIUuPvQrGf5ScMOE`,
+    });
     const response = this.http.post(
       'http://localhost:3010/api/createCourse',
-      course
+      course,
+      { headers: headers }
     );
     return response;
+  }
+
+  getAllCourses() {
+    // add custom header
+    //
+  }
+
+  getEnrolledCourses() {
+    // add custom header
+    // http call
   }
 }
